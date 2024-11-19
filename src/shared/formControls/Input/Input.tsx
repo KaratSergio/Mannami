ef61d4535c69fd71f1bf3196c9281347fab930da
Holovko Maksym <golovkomaksim852@gmail.com>
2024-11-19 13:05:16 +0200

    update:btn&input

--- src/shared/formControls/Input/Input.tsx
+++ src/shared/formControls/Input/Input.tsx
@@ -1,5 +1,9 @@
-const Input = () => {
-  return <div></div>;
+interface InputProps {
+  style?: string;
+}
+
+const Input = ({ style }: InputProps) => {
+  return <input className={`${style}`}></input>;
 };
 
 export default Input;
