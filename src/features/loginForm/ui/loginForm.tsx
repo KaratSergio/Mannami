b3ad8999eb4c9b7ca1bf7130450c48e38ba2c484
Holovko Maksym <golovkomaksim852@gmail.com>
2024-11-22 16:43:03 +0200

    add-login-page

---
+++ src/features/loginForm/ui/loginForm.tsx
@@ -0,0 +1,37 @@
+import { Button, Input } from '@shared/formControls';
+import { LoginFormInputs } from '../model/types';
+import validation from '@shared/formControls/validationSchemas';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(validation.loginSchema),
+  });
+
+  const onSubmit = (data: LoginFormInputs) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input {...register('email')} error={errors.email?.message} placeholder="email" type="text" />
+      <Input
+        {...register('password')}
+        type="password"
+        withPasswordToggled
+        error={errors.password?.message}
+        placeholder="password"
+      />
+      <Button type="submit">Send</Button>
+    </form>
+  );
+};
+
+export default LoginForm;
