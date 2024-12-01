1d96705e721741f2f072f5e945939683240f25af
KaratSergio <137483700+KaratSergio@users.noreply.github.com>
2024-12-01 16:52:06 +0000

    Deploying to gh-pages from @ KaratSergio/Mannami@29aa62ff2d1fef406ffc6f3f722349b38920be22 🚀

---
+++ assets/ErrorPage-a16bdf80.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-358204ec.js";const o=()=>r.jsx("div",{children:r.jsx("h1",{children:"404 page not found"})});export{o as default};
--- assets/ErrorPage-d33aea4e.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-17335e78.js";const o=()=>r.jsx("div",{children:r.jsx("h1",{children:"404 page not found"})});export{o as default};
